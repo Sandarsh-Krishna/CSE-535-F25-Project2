a4de6f42ea1cd4e3264736ed8af99ed17945e2d3
mmehta29 <149706767+mmehta29@users.noreply.github.com>
2025-10-29 02:40:07 -0700

    made UI changes to all the screens

---
+++ app/src/main/java/edu/asu/cse535/meseretictactoe/Luxe.kt
@@ -0,0 +1,21 @@
+package edu.asu.cse535.meseretictactoe
+
+import androidx.compose.ui.graphics.Color
+
+
+object Luxe {
+        val bgStart = Color(0xFF0B1324)
+        val bgEnd   = Color(0xFF111827)
+        val glass   = Color(0xCC0B1324)
+        val glassBorder = Color(0x44FFFFFF)
+        val textPrimary = Color(0xFFE5E7EB)
+        val textMuted   = Color(0xFF9CA3AF)
+        val line       = Color(0x334F5563)
+        val accent1    = Color(0xFF7C3AED)
+        val accent2    = Color(0xFF4F46E5)
+        val chipOutline = Color(0x334F46E5)
+        val tileFill    = Color(0x1AFFFFFF)
+        val tileBorder  = Color(0x33FFFFFF)
+        val xColor      = Color(0xFF60A5FA)
+        val oColor      = Color(0xFFA78BFA)
+}
